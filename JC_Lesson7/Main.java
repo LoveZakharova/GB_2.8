754d74812412266994ed70f00381280d25f2738f
ZakharovaLove <zakharovalove@mail.ru>
2021-11-16 18:43:21 +0300

    ref

--- JC_Lesson7/Main.java
+++ JC_Lesson7/Main.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        //WeatherResponse weatherResponse = new WeatherResponse();
         String cityCode = WeatherResponse.detectCityId("Kazan");
         System.out.println(cityCode);
         String forecast = WeatherResponse.getForecast(cityCode);
         System.out.println(forecast);
+
+        //System.out.println(weatherResponse.showWeather());
 
 
     }
--- JC_Lesson7/WeatherResponse.java
+++ JC_Lesson7/WeatherResponse.java
@@ -6,14 +6,26 @@ import okhttp3.OkHttpClient;
 import com.fasterxml.jackson.*;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.sqlite.JDBC;
 
 import java.io.IOException;
 import java.security.SecureRandom;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WeatherResponse {
     final static OkHttpClient okHttpClient = new OkHttpClient();
     final static ObjectMapper objectMapper = new ObjectMapper();
     final static String host = "dataservice.accuweather.com";
+    /*public final String PATH_DB = "jdbc:sqlite:C:\\Users\\RedmiBook\\IdeaProjects\\JavaCore\\src\\main\\resources\\Weather.db";
+
+    Connection connection;
+    public WeatherResponse() throws SQLException {
+        DriverManager.registerDriver(new JDBC());
+        connection = DriverManager.getConnection(PATH_DB);
+
+    }*/
 
     public static String detectCityId(String cityName) throws IOException {
         //по какому адресу (URL!!!) и с какими параметрами нужно отправить запрос GET
@@ -71,13 +83,55 @@ public class WeatherResponse {
         //распарсить запрос, в данном случае только тело ответа
         String json = response.body().string();
 
-        /*String city = objectMapper.readTree(json).get(0).at("/LocalizedName").asText();
-        System.out.println(city);
-        String code = objectMapper.readTree(json).get(0).at("/Key").asText();*/
-
-        return json;
+        String forecastsJson = objectMapper.readTree(json).at("/DailyForecasts").toString();
+        String dayForecast = objectMapper.readTree(forecastsJson).get(0).toString();
+        String date= objectMapper.readTree(dayForecast).
+                at("/Date").
+                asText();
+        String maxTemp= objectMapper.readTree(dayForecast).
+                at("/Temperature").
+                at("/Maximum").
+                at("/Value").
+                asText();
+        String minTemp= objectMapper.readTree(dayForecast).
+                at("/Temperature").
+                at("/Minimum").
+                at("/Value").
+                asText();
+        System.out.println(date + " " + maxTemp + " " + minTemp);
+        return forecastsJson;
 
     }
+
+    /*DayTemp dayTemp = new DayTemp(); //просит параметры, но они должны автоматически попадать
+    public void addDayTemp (DayTemp dayTemp) {
+        try(PreparedStatement statement = connection.prepareStatement(
+                "INSERT INTO Weather('date', 'maxTemp', 'minTemp')" +
+                        " VALUES(?, ?, ?)"
+        )) {
+            statement.setObject(1, dayTemp.date);
+            statement.setObject(2, dayTemp.maxTemp);
+            statement.setObject(3, dayTemp.minTemp);
+            statement.execute();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public List<DayTemp> showWeather() throws SQLException {
+        List<DayTemp> daysTemp = new ArrayList<>();
+        try(Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Weather");
+            while (resultSet.next()) {
+                DayTemp dayTemp = new DayTemp(); //параметры??
+                dayTemp.date = resultSet.getString("date");
+                dayTemp.maxTemp = resultSet.getFloat("maxTemp");
+                dayTemp.minTemp = resultSet.getFloat("minTemp");
+                daysTemp.add(dayTemp);
+            }
+        }
+        return daysTemp;
+    }*/
 
 
 
